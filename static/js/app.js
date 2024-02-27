b06c18be037548818c1a4d7f1a7619e2e73452c7
Andrey <152083348+tengenatari@users.noreply.github.com>
2024-02-27 14:36:46 +0500

    Add files via upload

---
+++ static/js/app.js
@@ -0,0 +1,35 @@
+var data = []
+var token = ""
+
+jQuery(document).ready(function () {
+    $('#btn-process').on('click', function () {
+        review = $('#txt_review').val()
+        $.ajax({
+            url: '/predict',
+            type: "post",
+            contentType: "application/json",
+            dataType: "json",
+            data: JSON.stringify({
+                "sentence": review
+            }),
+            beforeSend: function () {
+                $('.overlay').show()
+            },
+            complete: function () {
+                $('.overlay').hide()
+            }
+        }).done(function (jsondata, textStatus, jqXHR) {
+
+            $('#final-score').val(jsondata['response']['result'])
+        }).fail(function (jsondata, textStatus, jqXHR) {
+            alert(jsondata['responseJSON'])
+        });
+    })
+
+    $('#txt_review').keypress(function (e) {
+        if (e.which === 13) {
+            $('#btn-process').click()
+            e.preventDefault()
+        }
+    });
+})
